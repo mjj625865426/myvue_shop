6ce76a3cbd671f29847cf3bb7c852b8695f93d1d
majunjiao <1012548289@qq.com>
2021-02-27 16:23:21 +0800

    login

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header
+      >Header <el-button type="info" @click="logout">退出</el-button></el-header
+    >
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    open: true, //自动打开浏览器
+    port: 8888 //设置自定义端口号
+  }
+}
